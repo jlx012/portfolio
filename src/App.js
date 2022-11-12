2d689cd33625b4282629d0f0a4c8c1dc43c393e9
jlx012 <james.lee01230@gmail.com>
2022-11-11 22:56:07 -0600

    Deleted unnecessary images and currently working on Landing page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useMediaQuery from "./hooks/useMediaQuery";
 import NavBar from "./scenes/Navbar";
+import DotGroup from "./scenes/DotGroup";
+import Landing from "./scenes/Landing"
 
 function App() {
   const [ selectedPage, setSelectedPage ] = useState('home');
+  const [ isTopOfPage, setIsTopOfPage ] = useState(true);
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) setIsTopOfPage(true);
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    }
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <div className="app bg-deep-blue">
       <NavBar 
         selectedPage = {selectedPage} setSelectedPage = {setSelectedPage}
       />
-
+      <div className="w-5/6 mx-auto md:h-full">
+        {isAboveMediumScreens && (
+          <DotGroup 
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage}
+          />
+        )}
+        <Landing setSelectedPage={setSelectedPage} />
+      </div>
     </div>
   );
 }
